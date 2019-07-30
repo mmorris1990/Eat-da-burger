0e99538819114b72fe79cfa0cf933faecdb87436
Michael Morris <mmorris1990@gmail.com>
2019-07-30 18:41:40 -0500

    added dotenv module. wrote mysql connection in .env and added to gitignore. Wrote connection.js, will connect to db!

---
+++ config/connection.js
@@ -0,0 +1,21 @@
+// Dependencies
+require("dotenv").config();
+var mysql = require("mysql");
+
+// MySQL DB Connection Information (remember to change this with our specific credentials)
+var connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DB
+});
+
+// Initiate MySQL Connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
