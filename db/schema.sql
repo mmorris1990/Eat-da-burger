f2da79e65b3f6bc56ce51bd76567203c64363b42
Michael Morris <mmorris1990@gmail.com>
2019-07-30 18:06:26 -0500

    fixed schema & added primary key, wrote insert query to create 3 burgers

--- db/schema.sql
+++ db/schema.sql
@@ -2,11 +2,11 @@ DROP DATABASE IF EXISTS burgers_db;
 
 CREATE DATABASE burgers_db;
 
-USE DATABASE burgers_db;
+USE burgers_db;
 
 CREATE TABLE burgers (
-    id INT AUTO_INCREMENT NOT NULL,
-    burger_name VARCHAR (256) NOT NULL,
-    devoured BOOLEAN NOT NULL DEFAULT false
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR (255) NOT NULL,
+    devoured BOOLEAN NOT NULL DEFAULT false,
+    PRIMARY KEY (id)
 );
-
---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO burgers (burger_name) 
+VALUES ("Mushroom & Swiss"), ("Bacon Slider") , ("Cheddar Butter Burger");
