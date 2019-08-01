d43a269d40338eaddc62c06490184a36bbc4ced7
Michael Morris <mmorris1990@gmail.com>
2019-08-01 15:28:05 -0500

    created burger object with orm routes

---
+++ models/burger.js
@@ -0,0 +1,29 @@
+var orm = require("../config/orm.js");
+
+// Create the burger object
+var burger = {
+    // Select all burger table entries
+    selectAll: function (cb) {
+        orm.selectAll('burgers', function (res) {
+            cb(res);
+        });
+    },
+
+    // Insert a new burger into the table
+    // The variables cols and vals are arrays
+    insertOne: function (cols, vals, cb) {
+        orm.insertOne('burgers', cols, vals, function (res) {
+            cb(res);
+        });
+    },
+
+    // Update an existing burger (from not devoured to devoured)
+    // The objColVals is an object specifying columns as object keys with associated values
+    updateOne: function (objColVals, condition, cb) {
+        orm.updateOne('burgers', objColVals, condition, function (res) {
+            cb(res);
+        });
+    }
+};
+
+module.exports = burger;
