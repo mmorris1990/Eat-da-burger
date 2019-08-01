c5ec3fbc88b1ed7c6feeb34dd385522080329db4
Michael Morris <mmorris1990@gmail.com>
2019-08-01 15:11:18 -0500

    wrote orm routes, exported

--- config/orm.js
+++ config/orm.js
@@ -4,43 +4,88 @@ var connection = require("./connection.js");
 // ["?", "?", "?"].toString() => "?,?,?";
 function printQuestionMarks(num) {
     var arr = [];
-  
+
     for (var i = 0; i < num; i++) {
-      arr.push("?");
-    }
-  
+        arr.push("?");
+    };
+
     return arr.toString();
-  };
-  
-  // Helper function to convert object key/value pairs to SQL syntax
-  function objToSql(ob) {
+};
+
+// Helper function to convert object key/value pairs to SQL syntax
+function objToSql(ob) {
     var arr = [];
-  
+
     // loop through the keys and push the key/value as a string int arr
     for (var key in ob) {
-      var value = ob[key];
-      // check to skip hidden properties
-      if (Object.hasOwnProperty.call(ob, key)) {
-        // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
-        if (typeof value === "string" && value.indexOf(" ") >= 0) {
-          value = "'" + value + "'";
-        }
+        var value = ob[key];
+        // check to skip hidden properties
+        if (Object.hasOwnProperty.call(ob, key)) {
+            // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
 
-        arr.push(key + "=" + value);
-      }
+            arr.push(key + "=" + value);
+        }
     }
-  
+
     // translate array of strings to a single comma-separated string
     return arr.toString();
-  };
+};
 
 var orm = {
-    // Route to select all data based on devoured value
-    selectAll:
+    // Route to select all burger data 
+    selectAll: function (tableInput, cb) {
+        // Construct the query string that returns all rows from the target table
+        var queryString = "SELECT * FROM " + tableInput + ";";
+
+        // Perform the database query
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+
+            // Return results in callback
+            cb(result);
+        });
+    },
 
     // Route to add a burger
-    insertOne:
+    insertOne: function (table, cols, vals, cb) {
+        // Construct the query string that inserts a single row into the target table
+        var queryString = "INSERT INTO " + table + " (" + cols.toString() + ") " + "VALUES (" + printQuestionMarks(vals.length) + ") ";
+
+        console.log(queryString);
+
+        // Perform the database query
+        connection.query(queryString, vals, function (err, result) {
+            if (err) {
+                throw err;
+            }
+
+            // Return results in callback
+            cb(result);
+        });
+    },
 
     // Route to change burger from left to right
-    updateOne: 
+    updateOne: function (table, objColVals, condition, cb) {
+        // Construct the query string that updates a single entry in the target table
+        var queryString = "UPDATE " + table + " SET" + objToSql(objColVals) + " WHERE " + condition;
+
+        console.log(queryString);
+
+        // Perform the database query
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+
+            // Return results in callback
+            cb(result);
+        });
+    }
 };
+
+module.exports = orm;
